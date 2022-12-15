fb18a0d26d68ed95db476ee2baebf427918dff72
robin-dc <robindelacruz725@gmail.com>
2022-12-15 16:57:33 +0800

    first commit

---
+++ test/appTest.js
@@ -0,0 +1,37 @@
+const assert = require('chai').assert;
+const { addNumbers } = require('../app');
+// const sayHello = require('../app').sayHello
+// const addNumbers = require('../app').addNumbers
+const app = require('../app');
+
+// results
+sayHelloResult = app.sayHello()
+addNumbersResult = app.addNumbers(5,5)
+
+describe('App', function(){
+
+    describe('sayHello()', function(){
+        it('sayHello should return hello', function(){
+            // let result = app.sayHello()
+            assert.equal(sayHelloResult, 'hello')
+        })
+
+        it('sayHello should return a type string', function(){
+            // let result = app.sayHello()
+            assert.typeOf(sayHelloResult, 'string')
+        })
+    })
+
+    describe('addNumbers()', function(){
+        it('addNumbers should return a type number', function(){
+        // let result = app.addNumbers(5,5)
+        assert.typeOf(addNumbersResult, 'number')
+        })
+
+        it('addNumbers should be above 5', function(){
+            // let result = app.addNumbers(5,5)
+            assert.isAbove(addNumbersResult, 5)
+        })
+    })
+
+})
